c833fdd2e7311467116869a0587e89567507d0a5
Erik Mossberg <clicketyclack@users.noreply.github.com>
2019-06-17 20:05:02 +0200

    VENV + basic hello world.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # serious-business
 Super Basic Streaming Network Server (sbsns)
+
+
+# Setting up an environment.
+
+```bash
+> virtualenv -p python3 env_sbsns
+> source env_sbsns/bin/activate
+> 
+```
---
+++ srv/srv_main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019 Erik Mossberg
+
+This file is part of SeriousBusiness.
+
+SeriousBusiness is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SeriousBusiness is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import cherrypy
+
+class SeriousServer(object):
+    @cherrypy.expose
+    def index(self):
+        return "A serious serving of cherrypy!"
+
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.quickstart(SeriousServer())
