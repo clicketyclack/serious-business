a55e55363c472398bdd7a57fe01433cb49d8a3cb
Erik Mossberg <clicketyclack@users.noreply.github.com>
2019-06-20 23:49:46 +0200

    Implement basic content serving.

--- srv/srv_main.py
+++ srv/srv_main.py
@@ -30,11 +30,52 @@ class SeriousServer(object):
     def index(self):
         toreturn = "A serious serving of cherrypy!<br>"
 
-        files = os.listdir('media')
-        for fname in files:
+        fnames = os.listdir('media')
+
+        for fname in fnames:
             b64key = base64.b64encode(fname.encode('ascii')).decode('ascii')
-            toreturn += "%s %s<br>" % (fname, b64key)
+            toreturn += "<a href='./serve_content?fkey=%s'>%s</a><br>" % (b64key, fname)
         return toreturn
+
+    @cherrypy.expose
+    def serve_content(self, fkey):
+        """
+        Serves raw files. Allows b64-encoded filenames.
+        """
+        clearkey = None
+        try:
+            clearkey = base64.b64decode(fkey.encode('ascii')).decode('ascii')
+            print("Clearkey is now '%s'" % clearkey)
+        except Exception as ex:
+            print("Key %s not b64-decodable." % fkey)
+
+        from cherrypy.lib.static import serve_file
+        #return serve_file(media[fkey], "application/x-download", "attachment")
+
+
+        # File name MUST! be checked against a
+        # whilelist, only containing content in
+        # the media directory. Basic protection
+        # against directory traversal.
+        fname_whitelist = os.listdir('media')
+
+        if fkey in fname_whitelist:
+            fname = os.path.join('media', fkey)
+            fname = os.path.abspath(fname)
+            print("Statically serving '%s" % fname)
+            return serve_file(fname)
+
+        elif clearkey is not None and clearkey in fname_whitelist:
+            fname = os.path.join('media', clearkey)
+            fname = os.path.abspath(fname)
+            print("Statically serving '%s" % fname)
+            return serve_file(fname)
+
+        else:
+            cherrypy.response.headers['Content-Type'] = 'text/plain'
+            cherrypy.response.status=404
+            return "No such fkey"
+
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0'})
